01b9b1942d11cd9e5e3614ab41feac30e95bc925
Jellorine <110653373+Jellorine@users.noreply.github.com>
2024-11-19 16:11:13 +0000

    StudentTable layout and style has done

--- student_information_portal./src/components/StudentTable.js
+++ student_information_portal./src/components/StudentTable.js
@@ -1,7 +1,17 @@
+import '../assets/css/compo.css'
 export default function StudentTable(props){
     return(
-        <div>
-
+        <div className="outerDiv">
+            <div className="leftDiv">
+                <table>
+                    <tr>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>Course</td>
+                        <td>Country</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 }
