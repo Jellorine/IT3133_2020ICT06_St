f3f95b9dc0a9ac5c85e0f7fa4ddc719495ebc014
Jellorine <110653373+Jellorine@users.noreply.github.com>
2024-11-19 17:11:12 +0000

    view button onClick func added

--- student_information_portal./src/components/StudentTable.js
+++ student_information_portal./src/components/StudentTable.js
@@ -1,5 +1,8 @@
-import '../assets/css/compo.css'
+import '../assets/css/compo.css';
+import {useState} from 'react';
+
 export default function StudentTable(props){
+    const[student,setStudent] = useState(props.students[0]);
     return(
         <div className="outerDiv">
             <div className="leftDiv">
@@ -10,6 +13,7 @@ export default function StudentTable(props){
                             <td>Last Name</td>
                             <td>Course</td>
                             <td>Country</td>
+                            <td>Profile</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -20,11 +24,15 @@ export default function StudentTable(props){
                                     <td>{st.lastName}</td>
                                     <td>{st.course}</td>
                                     <td>{st.address.country}</td>
+                                    <td><button onClick={()=>{setStudent(student)}}>View</button></td>
                                 </tr>
                             )
                         }
                     </tbody>
                 </table>
+            </div>
+            <div className="rightDiv">
+                <Profile student={st}/>
             </div>
         </div>
     );
